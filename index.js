25c8b8c1e5b1e3f19e3664aed403be9a41535e8d
Paul Doronila <doronila@jpl.nasa.gov>
2018-12-11 07:36:50 -0800

    Adding support for Giiker i3s

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ class Giiker extends EventEmitter {
 
     const device = await window.navigator.bluetooth.requestDevice({
       filters: [{
-        namePrefix: 'GiC',
+        namePrefix: 'GiS',
       }],
       optionalServices: [SERVICE_UUID, SYSTEM_SERVICE_UUID],
     });
